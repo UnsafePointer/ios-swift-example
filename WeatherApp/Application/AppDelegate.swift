6d8626c55f972c8703ca58cf6a37a50d8d8b7c2f
Renzo Crisostomo <ruenzuo@gmail.com>
2014-08-12 11:31:45 +0200

    Initial commit

---
+++ WeatherApp/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Renzo Crisostomo on 12/08/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        return true
+    }
+
+}
+
---
+++ WeatherApp/Controllers/CountriesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Renzo Crisostomo on 12/08/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+import UIKit
+
+class CountriesViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
